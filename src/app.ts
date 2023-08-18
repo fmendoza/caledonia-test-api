4776a5f2664d4a508e650a1ea5ab8a83ad3b76bf
Fernando Mendoza <fernando@Fernandos-MacBook-Pro.local>
2023-08-18 15:39:39 -0600

    add parse support and deployment using github actions

--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,29 @@ import express, { Request, Response, NextFunction } from 'express';
 import ApplicationError from './errors/application-error';
 import routes from './routes';
 import logger from './logger';
+// @ts-expect-error
+import { ParseServer } from 'parse-server';
+// @ts-expect-error
+import ParseDashboard from 'parse-dashboard';
 
 const app = express();
 
 function logResponseTime(req: Request, res: Response, next: NextFunction) {
-    const startHrTime = process.hrtime();
+  const startHrTime = process.hrtime();
 
-    res.on('finish', () => {
-        const elapsedHrTime = process.hrtime(startHrTime);
-        const elapsedTimeInMs =
-            elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
-        const message = `${req.method} ${res.statusCode} ${elapsedTimeInMs}ms\t${req.path}`;
-        logger.log({
-            level: 'debug',
-            message,
-            consoleLoggerOptions: { label: 'API' },
-        });
+  res.on('finish', () => {
+    const elapsedHrTime = process.hrtime(startHrTime);
+    const elapsedTimeInMs =
+      elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+    const message = `${req.method} ${res.statusCode} ${elapsedTimeInMs}ms\t${req.path}`;
+    logger.log({
+      level: 'debug',
+      message,
+      consoleLoggerOptions: { label: 'API' },
     });
+  });
 
-    next();
+  next();
 }
 
 app.use(logResponseTime);
@@ -34,26 +38,60 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-    express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
+  express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
 );
 
 app.use(routes);
 
 app.use(
-    (
-        err: ApplicationError,
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        if (res.headersSent) {
-            return next(err);
-        }
-
-        return res.status(err.status || 500).json({
-            error: err.message,
-        });
+  (
+    err: ApplicationError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (res.headersSent) {
+      return next(err);
     }
+
+    return res.status(err.status || 500).json({
+      error: err.message,
+    });
+  }
 );
+
+const mountPath = process.env.PARSE_API_MOUNT || '/parse';
+const serverUrl = `${process.env.PARSE_API_SERVER_URL}:${process.env.PORT}${mountPath}`;
+
+const startParseServer = async () => {
+  
+  const server = new ParseServer({
+    databaseURI: process.env.MONGO_URL,
+    cloud: __dirname + '/cloud/main.ts',
+    appId: process.env.PARSE_API_APP_ID,
+    masterKey: process.env.PARSE_API_MASTER_KEY,
+    serverURL: serverUrl,
+  });
+
+  await server.start();
+
+  app.use(mountPath, server.app);
+}
+
+startParseServer();
+
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: serverUrl,
+      appId: process.env.PARSE_API_APP_ID,
+      masterKey: process.env.PARSE_API_MASTER_KEY,
+      appName: process.env.APP_NAME
+    }
+  ]
+});
+
+// mount parse-dashboard
+app.use(process.env.PARSE_DASHBOARD_MOUNT || '', dashboard);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ if (result.error) {
 
 import util from 'util';
 import app from './app';
-import SafeMongooseConnection from './lib/safe-mongoose-connection';
 import logger from './logger';
 
 const PORT = process.env.PORT || 3000;
@@ -25,18 +24,6 @@ if (process.env.NODE_ENV === 'development') {
   };
 }
 
-const safeMongooseConnection = new SafeMongooseConnection({
-  mongoUrl: process.env.MONGO_URL ?? '',
-  debugCallback,
-  onStartConnection: mongoUrl => logger.info(`Connecting to MongoDB at ${mongoUrl}`),
-  onConnectionError: (error, mongoUrl) => logger.log({
-    level: 'error',
-    message: `Could not connect to MongoDB at ${mongoUrl}`,
-    error
-  }),
-  onConnectionRetry: mongoUrl => logger.info(`Retrying to MongoDB at ${mongoUrl}`)
-});
-
 const serve = () => app.listen(PORT, () => {
   logger.info(`🌏 Express server started at http://localhost:${PORT}`);
 
@@ -50,10 +37,7 @@ if (process.env.MONGO_URL == null) {
   logger.error('MONGO_URL not specified in environment', new Error('MONGO_URL not specified in environment'));
   process.exit(1);
 } else {
-  safeMongooseConnection.connect(mongoUrl => {
-    logger.info(`Connected to MongoDB at ${mongoUrl}`);
-    serve();
-  });
+  serve();
 }
 
 // Close the Mongoose connection, when receiving SIGINT
@@ -61,15 +45,7 @@ process.on('SIGINT', async () => {
   console.log('\n'); /* eslint-disable-line */
   logger.info('Gracefully shutting down');
   logger.info('Closing the MongoDB connection');
-  try {
-    await safeMongooseConnection.close(true);
-    logger.info('Mongo connection closed successfully');
-  } catch (err) {
-    logger.log({
-      level: 'error',
-      message: 'Error shutting closing mongo connection',
-      error: err
-    });
-  }
+
+  // TODO: Close database
   process.exit(0);
 });
